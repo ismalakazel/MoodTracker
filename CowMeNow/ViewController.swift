b772ba70f1fecf68f1fdf14ea787e039203a7f63
Israel P. T. da Silva <ismalakazel@gmail.com>
2018-07-26 19:53:43 -0300

    Initial Commit

---
+++ CowMeNow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CowMeNow
+//
+//  Created by Israel P. T. da Silva on 26/07/18.
+//  Copyright © 2018 Israel P. T. da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
